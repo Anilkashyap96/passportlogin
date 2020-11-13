8c2e8ad03ae134d3e9d724887011e346c79a7619
Anil Kashyap <kashyapanil159@gmail.com>
2020-11-14 04:20:37 +0530

    Add files via upload
    passport login done

--- server.js
+++ server.js
@@ -1,89 +1,85 @@
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-}
-
-const express   = require('express')
-const app       = express()
-const bcrypt    = require('bcrypt')
-const flash     = require('express-flash')
-const session   = require('express-session')
-
-const passport  = require('passport')
-const initializePassport = require('./passport-config')
-initializePassport(
-    passport, 
-    (email) => {
-        return users.find(user => user.email === email)
-    }
-)
-
-const users = []
-
-app.set('view-engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
-app.use(flash())
-app.use(session({
+  }
+  
+  const express = require('express')
+  const app = express()
+  const bcrypt = require('bcrypt')
+  const passport = require('passport')
+  const flash = require('express-flash')
+  const session = require('express-session')
+  
+  const initializePassport = require('./passport-config')
+  initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+  )
+  
+  const users = []
+  
+  app.set('view-engine', 'ejs')
+  app.use(express.urlencoded({ extended: false }))
+  app.use(flash())
+  app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.get('/', checkAuthenticated, (req, res) =>{
-    res.render('index.ejs', {name:'Anil'})
-})
-
-app.get('/login', checkNotAuthenticated, (req, res)=>{
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+  
+  app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
+  })
+  
+  app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
-})
-
-app.get('/register',checkNotAuthenticated, (req, res)=>{
-    res.render('register.ejs')
-})
-
-app.post('/login',checkNotAuthenticated, passport.authenticate('local', {
+  })
+  
+  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/login',
     failureFlash: true
-}))
-app.post('/register', checkNotAuthenticated, async (req, res)=>{
-    const hashedPassword = await bcrypt.hash(req.body.password, 10)
-    console.log(hashedPassword)
+  }))
+  
+  app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+  })
+  
+  app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
-        users.push({
-            id:Date.now.toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch (error) {
-        console.log(error)
-        res.redirect('/register')
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
     }
-    console.log(users)
-})
-
-app.delete('/logout', (req, res) => {
-    req.logout()
+  })
+  
+  app.delete('/logout', (req, res) => {
+    req.logOut()
     res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next){
-    if(req.isAuthenticated){
-        return next()
+  })
+  
+  function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
     }
+  
     res.redirect('/login')
-}
-
-function checkNotAuthenticated(req, res, next){
-    if(req.isAuthenticated){
-       return res.redirect('/')
+  }
+  
+  function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
     }
     next()
-}
-
-app.listen(5000, function(){
-    console.log(`Server is running on port 5000`)
-})
+  }
+  
+  app.listen(3000)
